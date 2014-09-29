296262b906b744e9afd3f83b7315bf993eb43807
CuiVincent <xoyoshin@gmail.com>
2014-09-30 00:58:28 +0800

    Initial commit

---
+++ 006_ui_module1.py
@@ -0,0 +1,69 @@
+__author__ = 'cuizhe01'
+import tornado.web
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import os.path
+
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
+
+class HelloHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('hello.html', books=[
+                {
+                    "title":"AProgramming Collective Intelligence",
+                    "subtitle": "Building Smart Web 2.0 Applications",
+                    "image":"/static/images/collective_intelligence.gif",
+                    "author": "Toby Segaran",
+                    "date_added":1310248056,
+                    "date_released": "August 2007",
+                    "isbn":"978-0-596-52932-1",
+                    "description":"<p>This fascinating book demonstrates how you "
+                        "can build web applications to mine the enormous amount of data created by people "
+                        "on the Internet. With the sophisticated algorithms in this book, you can write "
+                        "smart programs to access interesting datasets from other web sites, collect data "
+                        "from users of your own applications, and analyze and understand the data once "
+                        "you've found it.</p>"
+                },{
+                    "title":"BProgramming Collective Intelligence",
+                    "subtitle": "Building Smart Web 2.0 Applications",
+                    "image":"/static/images/collective_intelligence.gif",
+                    "author": "Toby Segaran",
+                    "date_added":1310248056,
+                    "date_released": "August 2007",
+                    "isbn":"978-0-596-52932-1",
+                    "description":"<p>This fascinating book demonstrates how you "
+                        "can build web applications to mine the enormous amount of data created by people "
+                        "on the Internet. With the sophisticated algorithms in this book, you can write "
+                        "smart programs to access interesting datasets from other web sites, collect data "
+                        "from users of your own applications, and analyze and understand the data once "
+                        "you've found it.</p>"
+                }
+            ])
+
+class HelloModule(tornado.web.UIModule):
+    def render(self):
+        return '<h1>Hello, world!</h1>'
+class BookModule(tornado.web.UIModule):
+    def render(self,book):
+        return self.render_string('modules/book.html',book = book)
+    def embedded_javascript(self):
+        return "document.write(\"hi!\")"
+    def embedded_css(self):
+        return ".book {background-color:#F5F5F5}"
+    def css_files(self):
+        return "/static/css/newreleases.css"
+    def javascript_files(self):
+        return "https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/jquery-ui.min.js"
+
+if __name__ == '__main__':
+    tornado.options.parse_command_line()
+    app = tornado.web.Application(
+        handlers=[(r'/', HelloHandler)],
+        template_path=os.path.join(os.path.dirname(__file__), '006_ui_module1_res/templates'),
+        ui_modules={'Hello': HelloModule,'Book': BookModule}
+    )
+    server = tornado.httpserver.HTTPServer(app)
+    server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
